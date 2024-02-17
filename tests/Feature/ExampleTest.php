59c774839990f4333b1ca175eae48d01fcf1cf5c
Ruber Flores <ruberfc@gmail.com>
2024-02-17 18:07:24 -0500

    Creando nuevo servicio

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -40,4 +40,22 @@ class ExampleTest extends TestCase
             "description" => "ACEPTADA"
         ]);
     }
+
+    //curl  http://localhost:9000/api/boleta/VT0007
+    public function testFacturaRoute()
+    {
+        // Envía una solicitud GET a la ruta de la guía de remisión
+        $response = $this->get('/api/boleta/VT0007');
+
+        // Verifica que la respuesta sea exitosa (código 200)
+        $response->assertStatus(200);
+
+        // Verifica que la respuesta JSON sea exactamente la esperada
+        $response->assertExactJson([
+            "state" => true,
+            "accept" => true,
+            "code" => "0",
+            "description" => "La Factura numero F001-2, ha sido aceptada"
+        ]);
+    }
 }
